b6533a76942674adc62e6124407fd7b66d5a5f8d
ToyB-Chan <jasjason0otto@gmail.com>
2022-02-18 19:17:13 +0100

    Setted project up

---
+++ src/main/java/net/chat/to/clipboard/ChatToClipboard.java
@@ -0,0 +1,9 @@
+package net.chat.to.clipboard;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChatToClipboard implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }  
+}
